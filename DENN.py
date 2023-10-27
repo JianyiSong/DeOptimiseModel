cb7098a9d9901db1760d3c8fafaed4a3e6717d60
JianyiSong <135944601+JianyiSong@users.noreply.github.com>
2023-10-27 15:09:51 +0800

    Update and rename test_DE.py to DENN.py

--- DENN.py
+++ DENN.py
@@ -1,35 +1,29 @@
-import os
-
 import numpy as np
 import geatpy as ea
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import tensorflow as tf
-import pandas as pd
 import matplotlib.pylab as plt
 
-import my_function
-from data_set.figsettings import plt_head
 
 
-class MyProblem(ea.Problem):  # 继承Problem父类
+class MyProblem(ea.Problem):  # Inherit the Problem parent class
     def __init__(self, data, initpars):
-        name = 'ZDT1'  # 初始化name（函数名称，可以随意设置）
-        M = 1  # 初始化M（目标维数）
-        maxormins = [1] * M  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
-        Dim = len(initpars)  # 初始化Dim（决策变量维数）
-        varTypes = [0] * Dim  # 初始化varTypes（决策变量的类型，0：实数；1：整数）
-        lb = []  # 决策变量下界
-        ub = []  # 决策变量上界
+        name = 'ZDT1'  # Initialize name (function name, feel free to set it)
+        M = 1  # Initialize M (target dimension)
+        maxormins = [1] * M  # Initialize maxormins (list of target min-max markers, 1: minimize this target; -1: maximize this target)
+        Dim = len(initpars)  # Initialize Dim (Decision Variable Dimension)
+        varTypes = [0] * Dim  # Initialize varTypes (type of decision variable, 0: real; 1: integer)
+        lb = []  # Lower bounds on decision variables
+        ub = []  # Upper bounds on decision variables
         for i in range(Dim):
             lb.append(initpars[i][0])
             ub.append(initpars[i][1])
-        lbin = [1] * Dim  # 决策变量下边界
-        ubin = [1] * Dim  # 决策变量上边界
+        lbin = [1] * Dim
+        ubin = [1] * Dim
         self.initpars = initpars
         self.phase = data[:, 0]
         self.datay = data[:, 1] - np.mean(data[:, 1])
         self.cost = []
-        #self.my_plt()   #展示数据量减少效果
 
         self.xspace = np.linspace(0, 1, 100)
         self.sigma = np.diff(self.datay, 2).std() / np.sqrt(6)  # Estimated observation noise values
@@ -42,7 +36,7 @@ class MyProblem(ea.Problem):  # 继承Problem父类
         # results
         self.DE_trace = []
 
-        # 调用父类构造方法完成实例化
+        # Instantiation is accomplished by calling the parent class constructor
         ea.Problem.__init__(self, name, M, maxormins, Dim, varTypes, lb, ub, lbin, ubin)
 
     def predict(self, allpara):
@@ -76,7 +70,7 @@ class MyProblem(ea.Problem):  # 继承Problem父类
     def get_lcfit(self, phen):
         return self.predict(phen)
 
-    def aimFunc(self, pop):  # 目标函数
+    def aimFunc(self, pop):  # objective function
         cost = []
         x = pop.Chrom
         self.DE_trace.append(x)
@@ -85,23 +79,22 @@ class MyProblem(ea.Problem):  # 继承Problem父类
             noisey = self.getdata(x[i])
             # Calculating the likelihood function
             cost.append(0.5 * np.sum(np.log(2 * np.pi * self.sigma ** 2) + (output - noisey) ** 2 / (self.sigma ** 2)))
-        pop.ObjV = np.reshape(np.array(cost), (-1, 1))  # 计算目标函数值，赋值给pop种群对象的ObjV属性
+        pop.ObjV = np.reshape(np.array(cost), (-1, 1))  # Compute the objective function value to assign to the ObjV property of the pop population object
 
-def test_geat(data, initpars, MAXGEN, NIND, Encoding):
-    problem = MyProblem(data, initpars)  # 生成问题对象
-    # 构建算法
-    Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
-    population = ea.Population(Encoding, Field, NIND)  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
-    myAlgorithm = ea.soea_DE_best_1_bin_templet(problem, population)  # 实例化一个算法模板对象
-    # 求解
-    myAlgorithm.MAXGEN = MAXGEN  # 最大进化代数
-    myAlgorithm.mutOper.F = 0.5  # 差分进化中的参数F
-    myAlgorithm.recOper.XOVR = 0.7  # 重组概率
-    myAlgorithm.logTras = 1  # 设置每隔多少代记录日志，若设置成0则表示不记录日志
-    myAlgorithm.verbose = False  # 设置是否打印输出日志信息
-    myAlgorithm.drawing = 3  # 设置绘图方式（0：不绘图；1：绘制结果图；2：绘制目标空间过程动画；3：绘制决策空间过程动画）
-    """===========================调用算法模板进行种群进化========================"""
-    [BestIndi, population] = myAlgorithm.run()  # 执行算法模板，得到最优个体以及最后一代种群
+def geat(data, initpars, MAXGEN, NIND, Encoding):
+    problem = MyProblem(data, initpars)  # Generating Problem Objects
+    # Construction Algorithms
+    Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # Creating a region descriptor
+    population = ea.Population(Encoding, Field, NIND)  # Instantiate the population object (at this point the population has not been initialized, just completing the instantiation of the population object)
+    myAlgorithm = ea.soea_DE_best_1_bin_templet(problem, population)  # Instantiate an algorithm template object
+    myAlgorithm.MAXGEN = MAXGEN  # Maximum number of evolutionary generations
+    myAlgorithm.mutOper.F = 0.5  # The parameter F in differential evolution
+    myAlgorithm.recOper.XOVR = 0.7  # probability of reorganization
+    myAlgorithm.logTras = 1  # Set how many generations to log, if it is set to 0 it means no logging
+    myAlgorithm.verbose = False  # Set whether to print out log messages
+    myAlgorithm.drawing = 3  # Setting the plotting mode (0: no plotting; 1: plotting the results; 2: animating the process in target space; 3: animating the process in decision space)
+    """===========================Calling algorithmic templates for population evolution========================"""
+    [BestIndi, population] = myAlgorithm.run()  # Execute the algorithm template to obtain the optimal individuals as well as the final generation population
     phen = BestIndi.Phen[0]
 
     pre = problem.get_lcfit(phen)
@@ -122,24 +115,20 @@ def test_geat(data, initpars, MAXGEN, NIND, Encoding):
     ax.invert_yaxis()  # y-axis reversed
     plt.xlabel('phase', fontsize=18)
     plt.ylabel('mag', fontsize=18)
-    my_function.plt_minor_locator(ax)
     plt.show()
-    print('时间已过 %s 秒' % myAlgorithm.passTime)
     return BestIndi,population
 
 
 def parameters(initpars, MAXGEN, NIND):
-    path = '../MCMCNN-main/'
     file = 'KIC 6431545.txt'
-    data = np.loadtxt(path + file)
-    BestIndi,population = test_geat(data, initpars, MAXGEN, NIND, Encoding)
+    data = np.loadtxt(file)
+    BestIndi,population = geat(data, initpars, MAXGEN, NIND, Encoding)
 
 
 if __name__ == '__main__':
-    plt_head()
-    MAXGEN = 100  # 最大进化代数。
-    NIND = 30  # 种群规模
-    Encoding = 'RI'  # 编码方式
+    MAXGEN = 100  # Maximum number of evolutionary generations
+    NIND = 100  # population size
+    Encoding = 'RI'  # coding method
 
     initpars = [(4655 / 5850 - 0.8, 4655 / 5850 + 0.8),
                 (54.40 / 90 - 90 / 90, 54.40 / 90 + 90 / 90),
